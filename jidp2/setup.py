3617ad10e8c84db8c5b660a3d92e0861056480d1
hong-yh <hyinghui@umich.edu>
2020-06-30 13:59:04 -0400

    Added bash script
    run script to see web page; setup.py not able to run yet

---
+++ jidp2/setup.py
@@ -0,0 +1,29 @@
+"""
+JI-Datapipeline python package configuration.
+
+Cannot run this file yet.
+"""
+
+from setuptools import setup
+
+setup(
+    name='jidp2',
+    version='0.1.0',
+    packages=['jidp2'],
+    include_package_data=True,
+    install_requires=[
+        # 'arrow==0.15.5',
+        # 'bs4==0.0.1',
+        'Flask==1.1.1',
+        'html5validator==0.3.3',
+        # 'nodeenv==1.3.5',
+        'pycodestyle==2.5.0',
+        'pydocstyle==5.0.2',
+        'pylint==2.4.4',
+        # 'pytest==5.3.5',
+        # 'pytest-flask==0.15.1',
+        # 'requests==2.22.0',
+        # 'selenium==3.141.0',
+        # 'sh==1.12.14',
+    ],
+)
