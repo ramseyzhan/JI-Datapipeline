0ecc80956b6c8bc30e2ef0d7457939d345725196
hong-yh <hyinghui@umich.edu>
2020-07-21 17:41:04 -0400

    [Threshold feature] <Added threshold submission in index.jsx>

--- jidp2/js/main.jsx
+++ jidp2/js/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Anomaly from './anomaly';
+import Index from "./index";
 
 // This method is only called once
 ReactDOM.render(
-  <Anomaly url="/api/v1/m/" />,
+  <Index url="/api/v1/" />,
   document.getElementById('reactEntry'),
 );
