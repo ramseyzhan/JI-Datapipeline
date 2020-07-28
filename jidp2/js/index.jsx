1fd601adca55756a3044b3b2fd7679c47c536c04
hong-yh <hyinghui@umich.edu>
2020-07-28 16:03:24 -0400

    [CodeStyle] <Refactored eslint issues>

--- jidp2/js/index.jsx
+++ jidp2/js/index.jsx
@@ -6,7 +6,9 @@ class Index extends React.Component {
   constructor(props) {
     // Initialize mutable state
     super(props);
-    this.state = { email: '', threshold: 2, is_input: false, anomaly: '' };
+    this.state = {
+      email: '', threshold: 2, isInput: false, anomaly: '',
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
@@ -14,7 +16,7 @@ class Index extends React.Component {
 
   componentDidMount() {
     const { url } = this.props;
-    const { threshold, is_input } = this.state;
+    const { threshold, isInput } = this.state;
     console.log('INDEX url', url);
     fetch(url, { credentials: 'same-origin' })
       .then((response) => {
@@ -23,37 +25,36 @@ class Index extends React.Component {
       })
       .then((data) => {
         this.setState(() => ({
-          threshold: threshold,
-          is_input: is_input,
+          threshold,
+          isInput,
           anomaly: data.anomaly,
         }));
       })
       .catch((error) => console.log(error));
   }
 
+  // eslint-disable-next-line react/sort-comp
   handleEmailChange(event) {
     this.setState({
       email: event.target.value,
     });
-    console.log('is_input: ', this.state.is_input);
   }
 
   handleChange(event) {
     this.setState({
       threshold: event.target.value,
     });
-    console.log('is_input: ', this.state.is_input);
   }
 
   onFormSubmit(event) {
     event.preventDefault();
-    const {url} = this.props;
-    const {email, threshold, anomaly} = this.state;
+    const { url } = this.props;
+    const { email, threshold, anomaly } = this.state;
     console.log('email: ', email);
     fetch(url,
       {
         method: 'post',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: email, number: threshold }),
         credentials: 'same-origin',
       })
@@ -61,34 +62,36 @@ class Index extends React.Component {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
-      .then((data) => {
+      .then(() => {
         this.setState({
-          email: email,
-          threshold: threshold,
-          is_input: true,
-          anomaly: anomaly,
+          email,
+          threshold,
+          isInput: true,
+          anomaly,
         });
       })
       .catch((error) => console.log(error));
   }
 
   render() {
-    const { email, threshold, is_input, anomaly } = this.state;
-    if (!is_input) {
+    const {
+      email, threshold, isInput, anomaly,
+    } = this.state;
+    if (!isInput) {
       return (
         <div className="index">
-          <form  onSubmit={this.onFormSubmit}>
+          <form onSubmit={this.onFormSubmit}>
             <table>
               <tr>
 
                 <td>Enter email address for alerting messages: </td>
                 <td>
-                    <input type="text" value={email} onChange={this.handleEmailChange}/>
+                  <input type="text" value={email} onChange={this.handleEmailChange} />
                 </td>
 
                 <td>Customize threshold (1 to 10): </td>
                 <td>
-                    <input type="number" value={threshold} min="1" max="10" step="1" onChange={this.handleChange}/>
+                  <input type="number" value={threshold} min="1" max="10" step="1" onChange={this.handleChange} />
                 </td>
 
                 <td>
